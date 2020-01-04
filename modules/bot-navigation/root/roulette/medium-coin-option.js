8e3c32c5597a02c3f3b3fa7680c0c3e0c60020af
Andrey Khataev <andrey.hataev@balance-pl.ru>
2020-01-04 21:54:14 +0300

    fix roulette

--- modules/bot-navigation/root/roulette/medium-coin-option.js
+++ modules/bot-navigation/root/roulette/medium-coin-option.js
@@ -2,22 +2,24 @@ const Markup = require("node-vk-bot-api/lib/markup");
 const MenuOption = require("../../menu-option");
 
 class MediumCoinOption extends MenuOption {
-  chatMessage(botCtx) {
+  chatMessage() {
+    let message;
     if (this.ctx.isPositiveBalance()) {
       const price = 100; // TODO: into settings
-      const prize = Math.random() * 400_000_000;
-      return `
+      const prize = Math.random() * 400000000;
+      message = `
       🎰 Происходит открытие рулетки 🤑 Medium Coin за ${price} ₽, ожидайте!
       
       🎰 С рулетки 🤑 Medium Coin вы получили ${prize} VK Coins!
       `;
     } else {
-      return `
+      message = `
       ❗ У Вас недостаточно средств для открытия рулетки 🤑 Medium Coin!
 
       ➕ Пополните счёт, нажав "Пополнить RUB" в главном меню.
       `;
     }
+    return message;
   }
 
   get buttonMarkup() {
