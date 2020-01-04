3127b7a73ec8ef11c2ff2778ddfe812ec9b1ee21
Andrey Khataev <andrey.hataev@balance-pl.ru>
2020-01-04 18:48:19 +0300

    fix util.js

--- modules/utils.js
+++ modules/utils.js
@@ -2,29 +2,27 @@ const axios = require("axios");
 const qs = require("qs");
 const settings = require("./config");
 
-let Utils = function() {
+class Utils {
   // shorten url with vk api
-  this.shortenUrl = async function(url) {
+  static async shortenUrl(url) {
     try {
       const accessToken = settings.get("credentials.bot.access_token");
       const shortLinkUrl = settings.get("credentials.vk.utils_short_link_url");
-      data = qs.stringify({
-        url: url,
+      const data = qs.stringify({
+        url,
         v: "5.103",
         access_token: accessToken
       });
       const response = await axios.post(shortLinkUrl, data);
       if (response.data.error)
-        throw new Error(
-          `${response.data.error.error_code}: ${response.data.error.error_msg}`
-        );
+        throw new Error(`${response.data.error.error_code}: ${response.data.error.error_msg}`);
 
       return response.data.response.short_url;
     } catch (error) {
       console.error(error);
       return url;
     }
-  };
-};
+  }
+}
 
-module.exports = new Utils();
+module.exports = Utils;
