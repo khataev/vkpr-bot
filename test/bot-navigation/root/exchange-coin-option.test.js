73f5b8f5e347c42008855ef0b7f6881793e78011
Andrey Khataev <andrey.hataev@balance-pl.ru>
2020-01-07 22:57:36 +0300

    Add exchange tests

--- test/bot-navigation/root/exchange-coin-option.test.js
+++ test/bot-navigation/root/exchange-coin-option.test.js
@@ -1,9 +1,9 @@
 const { describe, it } = require("mocha");
 const { expect } = require("chai");
 const { Account, ExchangeRate } = require("@models");
-const BalanceOption = require("@menu-root/balance-option");
+const ExchangeOption = require("@menu-root/exchange-coin-option");
 const { context, dummyBotCtx } = require("@test/helpers/setup-context");
-const balanceOption = new BalanceOption(context, {});
+const exchangeOption = new ExchangeOption(context, {});
 const botCtx = dummyBotCtx(1);
 const dbSetup = async () => {
   await ExchangeRate.setExchangeRate(100, 50);
@@ -13,25 +13,29 @@ const dbCleanup = async () => {
   await Account.destroy({ where: {}, truncate: true });
 };
 
-describe("Balance Menu Option", () => {
+describe("Exchange Coin Menu Option", () => {
   beforeEach(dbSetup);
   afterEach(dbCleanup);
 
   it("creates account if it absent", async () => {
-    // TODO: rename chatMessage to 'action'
-    await balanceOption.chatMessage(botCtx);
+    await exchangeOption.chatMessage(botCtx);
     const account = await Account.findOne({ where: { vkId: 1 } });
     expect(account.vkId).to.be.equal(1);
   });
 
-  it("returns correct balance", async () => {
+  it("perorms exchange", async () => {
     expectedResult = `
-    💰 Ваш баланс:
-    ➕ 2 000 000.000 VK Coins (1.00 ₽)
-    ➕ 1.00 ₽ (1 000 000.000 VK Coins)
+    💱 Вы успешно обменяли 2 000 000.000 VK Coin на 1.00 RUB!
     `;
-    await Account.create({ vkId: 1, rubAmount: 100, coinAmount: 2000000000 });
-    const result = await balanceOption.chatMessage(botCtx);
+    const account = await Account.create({
+      vkId: 1,
+      rubAmount: 100,
+      coinAmount: 2000000000
+    });
+    const result = await exchangeOption.chatMessage(botCtx);
+    await account.reload();
     expect(result.trim()).to.be.equal(expectedResult.trim());
+    expect(account.coinAmount).to.be.equal("0");
+    expect(account.rubAmount).to.be.equal(200);
   });
 });
